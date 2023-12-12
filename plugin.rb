a7b2093701249520e0183e35107a74c3b0972701
Backroads4Me <118474493+Backroads4Me@users.noreply.github.com>
2023-12-11 21:28:05 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -8,46 +8,46 @@
 # It's a best practice for Ruby code to avoid unexpected modifications to strings.
 
 
-# name: automation-script-email
+# name: discourse-custom-automations
 # about: Send an email when a new post is created.
 # version: 1
 # authors: Backroads4me
 
 # This line creates a setting in the Discourse admin panel to enable or disable this plugin.
-#enabled_site_setting :send_email_on_new_post_enabled
+enabled_site_setting :discourse_custom_automations_enabled
 
 # 'after_initialize' block: This code runs after the Discourse application has been fully initialized.
 # It ensures that all features and plugins of Discourse are loaded and available.
-#after_initialize do
+after_initialize do
 
   # 'reloadable_patch' block: Allows parts of the code to be reloaded without restarting the entire server.
   # Useful for development and testing, making it easier to apply and test changes.
-  #reloadable_patch do
+  reloadable_patch do
 
     # Check if the DiscourseAutomation plugin is defined and available.
     # If it's not, the following code won't run.
-    #if defined?(DiscourseAutomation)
+    if defined?(DiscourseAutomation)
 
       # Event listener for the 'post_created' event.
       # When a new post is created, the enclosed block is executed.
-      #on(:post_created) do |post|
+      on(:post_created) do |post|
 
         # Fetch all automation instances that are enabled and set to trigger on 'post_created'.
-        #DiscourseAutomation::Automation
-         # .where(enabled: true, trigger: "post_created")
-          #.find_each do |automation|
+        DiscourseAutomation::Automation
+          .where(enabled: true, trigger: "post_created")
+          .find_each do |automation|
 
             # Trigger the automation with the specified kind and post data.
             # This line essentially activates the automation logic for each matching automation instance.
-           # automation.trigger!(
-            #  "kind" => "post_created",
-              #"post" => post,
-            #)
-          #end
-      #end
-    #end
-  #end
-#end
+            automation.trigger!(
+              "kind" => "post_created",
+              "post" => post,
+            )
+          end
+      end
+    end
+  end
+end
 
 
 # Define a constant for the script name. This is used to reference the script within the plugin.
@@ -55,9 +55,9 @@ DiscourseAutomation::Scriptable::SEND_EMAIL_ON_NEW_POST = "send_email_on_new_pos
 
 # Add a new automation script to DiscourseAutomation.
 # This part of the script defines what the automation does.
-#add_automation_scriptable(DiscourseAutomation::Scriptable::SEND_EMAIL_ON_NEW_POST) do
+add_automation_scriptable(DiscourseAutomation::Scriptable::SEND_EMAIL_ON_NEW_POST) do
 DiscourseAutomation::Scriptable.add(DiscourseAutomation::Scriptable::SEND_EMAIL_ON_NEW_POST) do
-version 1
+#version 1
 
   # Define the event that triggers this script.
   # 'post_created' means the script runs when a new post is made in the forum.
@@ -90,4 +90,4 @@ version 1
              subject: "test subject",
              body: "test body")
   end
-#end
+end
