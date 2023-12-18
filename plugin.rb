e0ddeec6867562bf5723a87c8694487a7ae5ce16
Backroads4Me <118474493+Backroads4Me@users.noreply.github.com>
2023-12-17 22:41:10 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -32,13 +32,15 @@ after_initialize do
         # This block defines what happens when the trigger event occurs.
         script do |context|
           
-rails.logger.add_with_opts(
+Rails.logger.add_with_opts(
 	::Logger::Severity::INFO,
 	"Script triggered",
 	"Custom Automation")
             
             # Retrieve the attributes of the object from the context provided by the trigger.
             post = context["post"]
+
+		Rails.logger.store.report(::Logger::Severity::INFO, "Script triggered", "Custom Automation")
    
             # Check if the post has any active flags
             if post.post_actions.where(post_action_type_id: 7, staff_took_action: false).exists?
